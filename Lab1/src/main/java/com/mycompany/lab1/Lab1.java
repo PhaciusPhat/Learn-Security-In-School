84eeab43981cfdda6db34426a028f088dc287598
phacius <yusuki2712@gmail.com>
2022-07-16 16:48:04 +0700

    finish lab 1

---
+++ Lab1/src/main/java/com/mycompany/lab1/Lab1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author yusuk
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.show();
+        
+    }
+}
